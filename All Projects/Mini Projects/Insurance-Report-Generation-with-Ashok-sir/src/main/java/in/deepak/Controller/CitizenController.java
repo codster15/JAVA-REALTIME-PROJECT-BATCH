6f1ade19f419fa587c57f0aae3a04fff51f3ac97
Deepak Singh <deepaksingh.desire@gmail.com>
2024-11-12 15:02:21 +0530

    add

--- All Projects/Mini Projects/Insurance-Report-Generation-with-Ashok-sir/src/main/java/in/deepak/Controller/CitizenController.java
+++ All Projects/Mini Projects/Insurance-Report-Generation-with-Ashok-sir/src/main/java/in/deepak/Controller/CitizenController.java
@@ -49,43 +49,4 @@ public class CitizenController {
 
 
 }
-//    @PostMapping("/submit")
-//    public String SubmitData(Model model,
-//                             @RequestParam (required = false) String plan,  // matches name="plan"
-//                             @RequestParam (required = false)  String status, // matches name="status"
-//                             @RequestParam (required = false)  String gender, // matches name="gender"
-//                             @RequestParam (required = false)  String startDate, // matches name="startDate"
-//                             @RequestParam (required = false) String endDate // matches name="endDate"
-//
-//
-//
-//                                                                                              ) {
-//        List<citizenEntity> allCitizen ;
-////        LocalDate start  ;
-////        LocalDate end ;
-//
-//        if(plan != null && status != null && gender != null && startDate != null && endDate != null){
-//
-//          LocalDate  start = LocalDate.parse(startDate);
-//           LocalDate end   = LocalDate.parse(endDate);
-//            SearchRequest searchObject = new SearchRequest();
-//            searchObject.setPlanName(plan);
-//            searchObject.setPlanStatus(status);
-//            searchObject.setGender(gender);
-//            searchObject.setStartDate(start);
-//            searchObject.setEndDate(end);
-//            SearchRequest search = reportServiceImpl.search(searchObject);
-//            System.out.println(searchObject);
-//        }else{
-//
-//
-//
-//            allCitizen = reportServiceImpl.findAllCitizen();
-//            allCitizen.forEach(e-> System.out.println(e));
-//            model.addAttribute("citizenList" , allCitizen);
-//
-//        }
-//
-//        return "index";
-//
-//    }
+
