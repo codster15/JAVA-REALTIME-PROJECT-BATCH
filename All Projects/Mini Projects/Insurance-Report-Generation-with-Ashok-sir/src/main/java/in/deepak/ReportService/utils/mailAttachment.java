dc9b54e6baa47e49868a840ee9cf43ebae6127b1
Deepak Singh <deepaksingh.desire@gmail.com>
2024-11-25 10:52:42 +0530

    add

--- All Projects/Mini Projects/Insurance-Report-Generation-with-Ashok-sir/src/main/java/in/deepak/ReportService/utils/mailAttachment.java
+++ All Projects/Mini Projects/Insurance-Report-Generation-with-Ashok-sir/src/main/java/in/deepak/ReportService/utils/mailAttachment.java
@@ -1,2 +1,14 @@
-package in.deepak.ReportService.utils;public class mailAttachment {
+package in.deepak.ReportService.utils;
+
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class mailAttachment {
+
+    public void mailSender(HttpServletResponse response ){
+
+    }
+
 }
