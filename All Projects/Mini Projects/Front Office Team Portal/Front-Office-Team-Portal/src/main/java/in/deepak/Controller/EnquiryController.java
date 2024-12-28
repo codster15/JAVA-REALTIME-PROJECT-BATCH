141b2fe5a78579d6409334d2f71d9845eac3eded
Deepak Singh <deepaksingh.desire@gmail.com>
2024-12-29 00:25:47 +0530

    add

--- All Projects/Mini Projects/Front Office Team Portal/Front-Office-Team-Portal/src/main/java/in/deepak/Controller/EnquiryController.java
+++ All Projects/Mini Projects/Front Office Team Portal/Front-Office-Team-Portal/src/main/java/in/deepak/Controller/EnquiryController.java
@@ -1,5 +1,19 @@
 package in.deepak.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class EnquiryController {
+
+    @GetMapping("/dashboard")
+    public String dashboard(){
+        return "dashboard";
+    }
+
+    @GetMapping("/addEnquiry")
+    public String addEnquiry(){
+        return "addEnquiry";
+    }
 
 }
--- All Projects/Mini Projects/Front Office Team Portal/Front-Office-Team-Portal/src/main/java/in/deepak/Controller/UserController.java
+++ All Projects/Mini Projects/Front Office Team Portal/Front-Office-Team-Portal/src/main/java/in/deepak/Controller/UserController.java
@@ -8,14 +8,12 @@ public class UserController {
 
     @GetMapping("/login")
     public String login() {
-
         return "login";
     }
 
 
     @GetMapping("/register")
     public String register() {
-
         return "register";
     }
 
